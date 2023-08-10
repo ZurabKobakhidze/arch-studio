cc9b211ec3e3dea8ee99b7af4dbf6738e855e783
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-08-10 18:45:33 +0400

    feat: import logo

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Header = () =>{
+return <HeaderElement></HeaderElement>
+}
+
+export default Header;
+
+const HeaderElement = styled.header`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
