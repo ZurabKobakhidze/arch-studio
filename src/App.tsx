adeecd5a3a9794a5790ba7c4d01624b2326249c1
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-08-10 17:04:25 +0400

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-
-
+import styled from "styled-components";
 
 function App() {
-  
-
   return (
     <>
-      <div>
-       
-      </div>
-     
+      <div></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const Main = styled.main`
+  background: #FFF;
+  width: 100%;
+  height: 100%;
+`;
